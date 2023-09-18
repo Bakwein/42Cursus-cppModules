68638d8cd50c08eeddf5a5d50246294a4941df9c
Bakwein <sefatuncaa3@gmail.com>
2023-09-18 20:47:10 +0300

    creation calismalar/reinterpret_cast 18.09-20.47

---
+++ cppModule06/calismalar/reinterpret_cast_2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    int id=65;
+    int *ip;
+
+    ip = &id;
+
+    // Farklı veri türünde işaretçiler arasında dönüşüm
+    char *cp = reinterpret_cast<char*>(ip);
+    std::cout << *cp << "\n";
+    
+    // Bir veri türünü aynı veri türünden bir işaretçiye dönüştürme
+    int *ip2 = reinterpret_cast<int*>(id);
+    std::cout << ip2 << "\n";
+    
+    // Bir işaretçiden bir veri türüne dönüşüm
+    long int lid = reinterpret_cast<long>(ip2);
+    std::cout << lid << "\n";
+
+}
